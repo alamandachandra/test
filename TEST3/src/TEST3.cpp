b64e3ea4e326b19186b7d37f1f39c3ed54aab3ad
Poorna-Chandra-Sekhar Alamanda <alamandachandra@gmail.com>
2013-05-22 15:38:18 +0200

    Modified the print statement in TEST3.cpp file.

--- TEST3/src/TEST3.cpp
+++ TEST3/src/TEST3.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World3!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World3 from TEST3.cpp!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
